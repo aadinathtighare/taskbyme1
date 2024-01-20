b2e580584679db17a174eb8a51672846a1cafc3a
devops <118253285+aadinathtighare@users.noreply.github.com>
2024-01-20 22:14:14 +0530

    Update lambda2_function.py

--- lambda2_function.py
+++ lambda2_function.py
@@ -4,3 +4,6 @@ import boto3
  
 my 2nd lambda plz pick
 i am change code in git but it ,, then it is same as it is update in myh lambda function....
+
+salman khan
+                                                                                        
